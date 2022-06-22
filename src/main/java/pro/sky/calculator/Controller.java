19a4f6c956a2d8088e82fca333f943a29377aa2f
nastyafallen <nastyafallen@rambler.ru>
2022-06-23 00:44:49 +0300

    Initial commit

---
+++ src/main/java/pro/sky/calculator/Controller.java
@@ -0,0 +1,67 @@
+package pro.sky.calculator;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/calculator")
+public class Controller {
+    private final ServiceCalculator serviceCalculator;
+
+    public Controller(ServiceCalculator serviceCalculator) {
+        this.serviceCalculator = serviceCalculator;
+    }
+
+    @GetMapping
+    public String welcome() {
+        return serviceCalculator.welcome();
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam(value = "num1", required = false) Float a,
+                       @RequestParam(value = "num2", required = false) Float b) {
+        if (Objects.isNull(a) || Objects.isNull(b)) {
+            return "Неправильно переданы параметры!";
+        }
+        return a + " + " + b + " = " + serviceCalculator.plus(a, b);
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam(value = "num1", required = false) Float a,
+                       @RequestParam(value = "num2", required = false) Float b) {
+        if (Objects.isNull(a) || Objects.isNull(b)) {
+            return "Неправильно переданы параметры!";
+        }
+        return a + " - " + b + " = " + serviceCalculator.minus(a, b);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam(value = "num1", required = false) Float a,
+                       @RequestParam(value = "num2", required = false) Float b) {
+        if (Objects.isNull(a) || Objects.isNull(b)) {
+            return "Неправильно переданы параметры!";
+        }
+        return a + " * " + b + " = " + serviceCalculator.multiply(a, b);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam(value = "num1", required = false) Float a,
+                       @RequestParam(value = "num2", required = false) Float b) {
+        if (Objects.isNull(a) || Objects.isNull(b)) {
+            return "Неправильно переданы параметры!";
+        }
+        if (a == 0 || b == 0) {
+            return "Ошибка! На ноль делить нельзя!";
+        } else {
+            return a + " / " + b + " = " + serviceCalculator.divide(a, b);
+        }
+    }
+
+}
+
+
---
+++ src/main/java/pro/sky/calculator/ServiceCalculator.java
@@ -0,0 +1,26 @@
+package pro.sky.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceCalculator {
+    public String welcome() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public Float plus(Float a, Float b) {
+        return a + b;
+    }
+
+    public Float minus(Float a, Float b) {
+        return a - b;
+    }
+
+    public Float multiply(Float a, Float b) {
+        return a * b;
+    }
+
+    public Float divide(Float a, Float b) {
+        return a / b;
+    }
+}
